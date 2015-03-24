e61256b0f751cae07986363ada0775b4f330f7f2
cpachomski <cpachomski@gmail.com>
2015-03-24 19:46:58 -0400

    add auth to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:index, :show]
 
   # GET /posts
   # GET /posts.json
@@ -66,6 +66,12 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |name, password|
+        name == 'admin' && password = "secret"
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
